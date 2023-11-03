e4dee09023c5d7abc99da971a594e7dd5be81270
meowsl <VlaFrank@ya.ru>
2023-11-03 10:00:53 +0300

    update

--- ahaha.py
+++ ahaha.py
@@ -1,15 +1,18 @@
-import fitz, os, sys
+import fitz
+import os, sys
 from PyPDF2 import PdfReader
 import pytesseract
 from pdf2image import convert_from_path
 from typing import Tuple
 from PIL import Image
-
+import config
 
 
 def extract_img(filename):
     img =Image.open(filename)
     text = pytesseract.image_to_string(img, lang='rus')
+    text = text.replace("\r", " ")
+    text = text.replace("\n", " ")
     print(text)
     os.remove(filename)
 
@@ -29,8 +32,6 @@ def convert_pdf(filename):
     doc.close()
 
 if __name__ == "__main__":
-    pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-    # custom_config = r'--oem 3 --psm 13'
-
-    file = 'reglament_zpps'
+    pytesseract.pytesseract.tesseract_cmd = config.pytesseract_path
+    file = input()
     convert_pdf(file)
