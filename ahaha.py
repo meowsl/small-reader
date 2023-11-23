bd103b381d4fd6b278ec5178dbdefdd19d926c91
meowsl <VlaFrank@ya.ru>
2023-11-23 14:25:49 +0300

    add comments

--- ahaha.py
+++ ahaha.py
@@ -9,29 +9,42 @@ import config
 
 
 def extract_img(filename):
-    img =Image.open(filename)
-    text = pytesseract.image_to_string(img, lang='rus')
+    '''
+    ОБРАБОТКА ТЕКСТА С PNG
+    '''
+    img =Image.open(filename) # Открытие файла png в переменной
+    text = pytesseract.image_to_string(img, lang='rus') # Конвертация в строку
+    # Удаление лишних символов
     text = text.replace("\r", " ")
     text = text.replace("\n", " ")
+
+    # Тут вместо вывода текста нужно будет сделать, чтобы текст передавался в модель джанги этим я сам займусь
     print(text)
     os.remove(filename)
 
 def convert_pdf(filename):
-    reader = PdfReader(f'inputs/{filename}.pdf')
-    count_page = reader.pages
+    '''
+    КОНВЕРТАЦИЯ ФАЙЛА В ПНГ ФОРМАТ
+    И СОХРАНЕНИЕ В ДИРЕКТОРИИ
+    '''
 
-    pdffile = f"inputs/{filename}.pdf"
+    reader = PdfReader(f'inputs/{filename}.pdf') # Открытие файла pdf в переменной
+    count_page = reader.pages # Подсчет страниц файла
+    pdffile = f"inputs/{filename}.pdf" # Путь до файла
     doc = fitz.open(pdffile)
-    for i in range(len(count_page)):
+    for i in range(len(count_page)): # Цикл для обработки всех страниц
         page = doc.load_page(i)  # number of page
-        pix = page.get_pixmap()
-        output = f"outfile{i}.png"
-        pix.save(output)
-        extract_img(output)
+        pix = page.get_pixmap() # Конвертация pdf страницы в png
+        output = f"outfile{i}.png" # Имя файла на выходе
+        pix.save(output) # Сохранения png в директории
+        extract_img(output) # Вызов функции
 
     doc.close()
 
 if __name__ == "__main__":
+    '''
+    Запуск программы
+    '''
     pytesseract.pytesseract.tesseract_cmd = config.pytesseract_path
-    file = input()
+    file = input() # указание имени файла
     convert_pdf(file)
